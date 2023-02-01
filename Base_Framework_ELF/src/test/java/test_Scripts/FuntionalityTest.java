6b70a16de3ebb3edec2862918185c91fc108d565
YogitaB-TY <yogita.b@testyantra.com>
2023-02-01 11:31:02 +0530

    Functionality test case added

---
+++ Base_Framework_ELF/src/test/java/test_Scripts/FuntionalityTest.java
@@ -0,0 +1,14 @@
+package test_Scripts;
+
+import org.testng.annotations.Test;
+
+import generic.Base_Test;
+
+public class FuntionalityTest extends  Base_Test{
+	
+	@Test
+	public void test() {
+		System.out.println("Functionality test case");
+	}
+
+}
